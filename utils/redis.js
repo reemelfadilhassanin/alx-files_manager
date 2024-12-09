45556a9e5cf53094e88a6805bed91ef01feef019
reemelfadilhassanin <justreemjust49@gmail.com>
2024-12-09 09:09:37 +0300

    try to fix 1

--- utils/redis.js
+++ utils/redis.js
@@ -1,45 +1,62 @@
-import redis from 'redis';
+// utils/redis.js
+const redis = require('redis');
 
 class RedisClient {
   constructor() {
+    // Create Redis client and connect to the default Redis server (localhost:6379)
     this.client = redis.createClient();
+
+    // Log any error that occurs during the connection
     this.client.on('error', (err) => {
-      console.log('Redis error: ', err);
+      console.error('Redis Client Error:', err);
     });
   }
 
+  // Checks if the Redis client is alive
   isAlive() {
     return this.client.connected;
   }
 
+  // Asynchronous function to get a value for a given key
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, result) => {
-        if (err) reject(err);
-        resolve(result);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
       });
     });
   }
 
+  // Asynchronous function to set a key-value pair with an expiration time
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, duration, value, (err, result) => {
-        if (err) reject(err);
-        resolve(result);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
       });
     });
   }
 
+  // Asynchronous function to delete a key
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (err, result) => {
-        if (err) reject(err);
-        resolve(result);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
       });
     });
   }
 }
 
-// Export an instance of RedisClient
+// Create and export a single instance of RedisClient
 const redisClient = new RedisClient();
-export default redisClient;
+module.exports = redisClient;
