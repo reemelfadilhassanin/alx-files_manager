b740a101c5924f9675bdbd2bc395a4afa2a87058
root <root@Reem.localdomain>
2024-12-09 12:22:53 +0300

    db done

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,34 @@
-import express from 'express';
-import AppController from '../controllers/AppController.js';  // Import the AppController
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const router = Router();
 
-// Define the /status endpoint
+// AppController endpoint definitions
 router.get('/status', AppController.getStatus);
-
-// Define the /stats endpoint
 router.get('/stats', AppController.getStats);
+
+// UsersController endpoint definitions
+router.post('/users', UsersController.postNew);
+
+// AuthController endpoint definitions
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+
+// FilesController endpoint definitions
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
+// Publishing and Unpublishing FilesController endpoint definitions
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+// File data endpoint definition
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import routes from './routes/index.js';  // Import routes from routes/index.js
+import routes from './routes';
 
-// Set up the Express app
 const app = express();
-
-// Set up the server to listen on PORT or default to 5000
 const PORT = process.env.PORT || 5000;
+
+// parse JSON request bodies
+app.use(express.json());
+// load routes
+app.use('/', routes);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// Load all the routes from routes/index.js
-app.use('/api', routes);  // Prefix all routes with /api
