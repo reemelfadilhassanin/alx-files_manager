691b13616388200c12508efc81775960526e3565
root <root@Reem.localdomain>
2024-12-09 12:25:54 +0300

    db done

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,27 +1,31 @@
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import redisClient from '../utils/redis.js';  // Import Redis client
+import dbClient from '../utils/db.js';  // Import MongoDB client
 
-const AppController = {
-  async getStatus(req, res) {
+class AppController {
+  // GET /status: Check if Redis and DB are alive
+  static async getStatus(req, res) {
+    // Check if Redis and DB are alive
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
 
-    if (redisStatus && dbStatus) {
-      res.status(200).json({ redis: true, db: true });
-    } else {
-      res.status(500).json({ redis: redisStatus, db: dbStatus });
-    }
-  },
+    // Return the status in the response
+    return res.status(200).json({ redis: redisStatus, db: dbStatus });
+  }
 
-  async getStats(req, res) {
+  // GET /stats: Get the number of users and files in the DB
+  static async getStats(req, res) {
     try {
+      // Get the number of users and files from the DB
       const usersCount = await dbClient.nbUsers();
       const filesCount = await dbClient.nbFiles();
-      res.status(200).json({ users: usersCount, files: filesCount });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+
+      // Return the counts in the response
+      return res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (err) {
+      // If an error occurs, return a 500 status with the error message
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
-  },
-};
+  }
+}
 
 export default AppController;
