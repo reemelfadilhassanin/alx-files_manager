26cc111ad71b6bf3f039b144d3a2015665ca010a
root <root@Reem.localdomain>
2024-12-09 11:45:28 +0300

    tying

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,15 @@
+.js
+Click to show/hide file contents
+
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
         },
-      },
+      ],
     ],
-  ],
 };
